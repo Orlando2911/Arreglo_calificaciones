62ea4ccdf8f1129f104487b3459af3b0fbe5dd2d
Orlando <orlando@pricode.io>
2020-07-06 18:46:05 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Calificaciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese cantidad de califaciones");
+            int[] calificaciones = new int[Convert.ToInt32(Console.ReadLine())];
+
+            int suma = 0;
+            for(int i = 0; i < calificaciones.Length; i++)
+            {
+                Console.WriteLine("Ingrese calificaciones {0}", i);
+                calificaciones[i] = Convert.ToInt32(Console.ReadLine());
+                suma += calificaciones[i];
+            }
+
+            double promedio = suma / calificaciones.Length;
+            Console.WriteLine("El promedio es {0}", promedio);
+
+        }
+ 
+    }
+}
